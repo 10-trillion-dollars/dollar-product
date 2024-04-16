ee9e5ae5d291b01dfbc60b13134f466c6f450c9b
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-16 16:30:01 +0900

    :recycle: 테스트용 Feign 도메인 주석처리

--- src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "dollar-order", url = "https://order.10-trillon-dollars.com/external")
-
+//@FeignClient(name = "dollar-order", url = "http://localhost:8084/external")
 public interface FeignOrderClient {
 
     @GetMapping("/{productId}/orderDetails")
--- src/main/java/org/example/dollarproduct/feign/FeignUserClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignUserClient.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "dollar-user", url = "https://user.10-trillon-dollars.com/external")
+//@FeignClient(name = "dollar-user", url = "http://localhost:8082/external")
 public interface FeignUserClient {
 
     @GetMapping("/users/{userId}")

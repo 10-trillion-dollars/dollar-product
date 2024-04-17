d7a51ec9379bf8b39a1e29fce2e03f3f45c541d1
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-17 14:36:24 +0900

    :sparkles: 저장, 수정에만 retry 적용으로 변경 및 recovery 적용

--- src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
@@ -1,11 +1,17 @@
 package org.example.dollarproduct.feign;
 
+import feign.FeignException;
 import feign.FeignException.FeignClientException;
+import java.util.Collections;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.example.dollarproduct.entity.Order;
 import org.example.dollarproduct.entity.OrderDetail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,15 +20,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name = "dollar-order", url = "http://localhost:8084/external")
 public interface FeignOrderClient {
 
+
     @GetMapping("/{productId}/orderDetails")
     @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
-        , noRetryFor = {FeignClientException.class}
+        , noRetryFor = FeignClientException.class
+        , recover = "recoverFindOrderDetailsByProductId"
     )
     List<OrderDetail> findOrderDetailsByProductId(@PathVariable Long productId);
 
+    @Recover
+    default List<OrderDetail> recoverFindOrderDetailsByProductId(FeignException e) {
+        Logger logger = LoggerFactory.getLogger(FeignOrderClient.class);
+        logger.error("All retries failed., error = {}", e.getMessage());
+        return null;
+    }
+
     @GetMapping("/orders/{orderId}")
-    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
-        , noRetryFor = {FeignClientException.class}
-    )
     Order getById(@PathVariable Long orderId);
+
+
 }
+
+

c1e045854b3a26447f7fdeaf96f598dd6d069515
SerenityZenDev <tjrqo2000@gmail.com>
2024-04-17 11:20:19 +0900

    :sparkles: Feign 요청 실패를 대비한 retry 구현

--- src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
@@ -1,9 +1,12 @@
 package org.example.dollarproduct.feign;
 
+import feign.FeignException.FeignClientException;
 import java.util.List;
 import org.example.dollarproduct.entity.Order;
 import org.example.dollarproduct.entity.OrderDetail;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,8 +15,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface FeignOrderClient {
 
     @GetMapping("/{productId}/orderDetails")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     List<OrderDetail> findOrderDetailsByProductId(@PathVariable Long productId);
 
     @GetMapping("/orders/{orderId}")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     Order getById(@PathVariable Long orderId);
 }
--- src/main/java/org/example/dollarproduct/feign/FeignUserClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignUserClient.java
@@ -1,7 +1,10 @@
 package org.example.dollarproduct.feign;
 
+import feign.FeignException.FeignClientException;
 import org.example.share.config.global.entity.user.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,5 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 public interface FeignUserClient {
 
     @GetMapping("/users/{userId}")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 1000, maxDelay = 5000)
+        , noRetryFor = {FeignClientException.class}
+    )
     User findById(@PathVariable Long userId);
 }

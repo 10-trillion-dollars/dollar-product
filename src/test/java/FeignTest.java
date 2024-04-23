958779092a725e6fe948a3274722371eb31957f7
tyler <smooky0702@gmail.com>
2024-04-23 12:17:34 +0900

    :recycle:테스트 명칭 변경

--- src/test/java/FeignTest.java
+++ src/test/java/FeignTest.java
@@ -1,19 +1,15 @@
 import org.example.dollarproduct.entity.Order;
-import org.example.dollarproduct.entity.OrderDetail;
 import org.example.share.config.global.entity.user.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 import org.assertj.core.api.Assertions;
 
-import java.util.List;
+public class FeignTest {
 
-public class ProductTest {
-
-    //실행하기 전에 FeignClient interface에 url이 local인지 확인
+    //실행하기 전에 FeignClient에 url이 local인지 확인
     //각 서버가 실행이 되고 있는지 확인
     @Test
     @DisplayName("Order 통신 테스트")

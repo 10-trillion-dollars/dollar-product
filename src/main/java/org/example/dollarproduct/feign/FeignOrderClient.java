8fc76b468f1dc27be0bfb655a50ed456986c6f8a
JooJuneHo <jw059@naver.com>
2024-04-26 10:15:44 +0900

    :recycle: main branch pull

--- src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
+++ src/main/java/org/example/dollarproduct/feign/FeignOrderClient.java
@@ -1,24 +1,17 @@
 package org.example.dollarproduct.feign;
 
-import feign.FeignException;
-import feign.FeignException.FeignClientException;
-import java.util.Collections;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.example.dollarproduct.entity.Order;
 import org.example.dollarproduct.entity.OrderDetail;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Recover;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-//@FeignClient(name = "dollar-order", url = "https://order.10-trillon-dollars.com/external")
-@FeignClient(name = "dollar-order", url = "http://localhost:8084/external")
+
+@FeignClient(name = "dollar-order", url = "${loadbalancer.order}/external")
+//@FeignClient(name = "dollar-order", url = "http://localhost:8084/external")
+
 public interface FeignOrderClient {
 
 
